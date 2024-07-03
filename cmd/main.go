017ef03003f4bee4c81f9950ef68520e40cc3bce
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-07-04 00:10:20 +0200

    refactor: migrate from grpc.DialContext to NewClient

--- cmd/main.go
+++ cmd/main.go
@@ -121,12 +121,12 @@ func getServiceDependencies(ctx context.Context, serviceName string, port int, i
 		articleCreds = credentials.NewTLS(tlsConfig)
 	}
 
-	userConn, err := grpc.DialContext(ctx, "user-service:50051", grpc.WithTransportCredentials(userCreds), grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
+	userConn, err := grpc.NewClient("user-service:50051", grpc.WithTransportCredentials(userCreds), grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
 	if err != nil {
 		return service.Dependencies{}, err
 	}
 
-	articleConn, err := grpc.DialContext(ctx, "article-service:50051", grpc.WithTransportCredentials(articleCreds), grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
+	articleConn, err := grpc.NewClient("article-service:50051", grpc.WithTransportCredentials(articleCreds), grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
 	if err != nil {
 		return service.Dependencies{}, err
 	}
