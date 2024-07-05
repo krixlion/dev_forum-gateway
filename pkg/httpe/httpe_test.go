f5368913a7867357ed6fee4eb242b057319077a3
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-07-05 20:06:51 +0200

    test: add missing tests for httpe package

--- pkg/httpe/httpe_test.go
+++ pkg/httpe/httpe_test.go
@@ -1,30 +1,114 @@
 package httpe
 
 import (
+	"errors"
+	"io"
 	"net/http"
-	"reflect"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/krixlion/dev_forum-lib/logging"
+	"github.com/krixlion/dev_forum-lib/nulls"
 )
 
 func TestNewHandlerFunc(t *testing.T) {
 	type args struct {
 		fn     HandlerEFunc
 		logger logging.Logger
+		r      *http.Request
+	}
+	type result struct {
+		statusCode int
+		body       string
+		headers    map[string]string
 	}
 	tests := []struct {
 		name string
 		args args
-		want http.HandlerFunc
+		want result
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test handler sends unchanged response on no error",
+			args: args{
+				r: httptest.NewRequest("POST", "/", nil),
+				fn: func(r *http.Request) (Response, error) {
+					return NewResponse(http.StatusOK, map[string]string{"testdata": "test"}), nil
+				},
+				logger: nulls.NullLogger{},
+			},
+			want: result{
+				statusCode: http.StatusOK,
+				body:       `{"testdata":"test"}` + "\n",
+				headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+			},
+		},
+		{
+			name: "Test handler sends unchanged error response on HTTP error",
+			args: args{
+				r: httptest.NewRequest("POST", "/", nil),
+				fn: func(r *http.Request) (Response, error) {
+					return nil, HttpError{
+						status: http.StatusBadRequest,
+						msg:    "test err msg",
+					}
+				},
+				logger: nulls.NullLogger{},
+			},
+			want: result{
+				statusCode: http.StatusBadRequest,
+				body:       `{"error":"test err msg"}` + "\n",
+				headers: map[string]string{
+					"Content-Type":           "application/json",
+					"X-Content-Type-Options": "nosniff",
+				},
+			},
+		},
+		{
+			name: "Test handler sends generic 500 response on non HTTP errors",
+			args: args{
+				r: httptest.NewRequest("POST", "/", nil),
+				fn: func(r *http.Request) (Response, error) {
+					return nil, errors.New("test err")
+				},
+				logger: nulls.NullLogger{},
+			},
+			want: result{
+				statusCode: http.StatusInternalServerError,
+				body:       `{"error":"Internal Server Error"}` + "\n",
+				headers: map[string]string{
+					"Content-Type":           "application/json",
+					"X-Content-Type-Options": "nosniff",
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewHandlerFunc(tt.args.fn, tt.args.logger); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewHandlerFunc():\n got = %v\n want = %v", got, tt.want)
+			w := httptest.NewRecorder()
+
+			NewHandlerFunc(tt.args.fn, tt.args.logger).ServeHTTP(w, tt.args.r)
+
+			got := w.Result()
+			if got.StatusCode != tt.want.statusCode {
+				t.Errorf("NewHandler.ServeHTTP(): unexpected status code:\n got = %v\n want = %v", got.StatusCode, tt.want.statusCode)
+			}
+
+			for key, want := range tt.want.headers {
+				if got := got.Header.Get(key); got != want {
+					t.Errorf("NewHandler.ServeHTTP(): unexpected header:\n got = %v\n want = %v\n", got, want)
+				}
+			}
+
+			body, err := io.ReadAll(got.Body)
+			if err != nil {
+				t.Fatalf("NewHandler.ServeHTTP():\n failed to read response body = %v\n", err)
+			}
+
+			if v := string(body); v != tt.want.body {
+				t.Errorf("NewHandler.ServeHTTP():\n got = %v\n want = %v", v, tt.want.body)
 			}
 		})
 	}
@@ -228,41 +312,141 @@ func TestNewError(t *testing.T) {
 
 func Test_respondError(t *testing.T) {
 	type args struct {
-		w   http.ResponseWriter
 		err HttpError
+	}
+	type result struct {
+		statusCode int
+		body       string
 	}
 	tests := []struct {
 		name    string
 		args    args
+		want    result
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test err response is sent with no errors",
+			args: args{
+				err: HttpError{
+					status: http.StatusInternalServerError,
+					msg:    "InternalServerError",
+				},
+			},
+			want: result{
+				statusCode: http.StatusInternalServerError,
+				body:       `{"error":"InternalServerError"}` + "\n",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := respondError(tt.args.w, tt.args.err); (err != nil) != tt.wantErr {
-				t.Errorf("respondError():\n error = %v\n wantErr = %v", err, tt.wantErr)
+			w := httptest.NewRecorder()
+
+			if err := respondError(w, tt.args.err); (err != nil) != tt.wantErr {
+				t.Fatalf("respondError():\n error = %v\n wantErr = %v", err, tt.wantErr)
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			got := w.Result()
+			if got.StatusCode != tt.want.statusCode {
+				t.Errorf("respondError(): unexpected status code:\n got = %v\n want = %v", got.StatusCode, tt.want.statusCode)
+			}
+
+			headers := map[string]string{
+				"Content-Type":           "application/json",
+				"X-Content-Type-Options": "nosniff",
+			}
+
+			for key, want := range headers {
+				if got := got.Header.Get(key); got != want {
+					t.Errorf("respondError(): unexpected header:\n got = %v\n want = %v\n", got, want)
+				}
+			}
+
+			body, err := io.ReadAll(got.Body)
+			if err != nil {
+				t.Fatalf("respondError():\n failed to read response body = %v\n", err)
+			}
+
+			if v := string(body); v != tt.want.body {
+				t.Errorf("respondError():\n got = %v\n want = %v", v, tt.want.body)
 			}
 		})
 	}
 }
 
 func Test_respond(t *testing.T) {
-	type args struct {
-		w    http.ResponseWriter
-		resp Response
+	type result struct {
+		statusCode int
+		body       string
 	}
 	tests := []struct {
 		name    string
-		args    args
+		resp    Response
+		want    result
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test empty response with status 200 is sent with no errors",
+			resp: NewResponse(http.StatusOK, nil),
+			want: result{
+				statusCode: http.StatusOK,
+				body:       "null\n",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test encodes json response with no errors",
+			resp: NewResponse(http.StatusCreated, map[string]string{
+				"data": "testdata",
+			}),
+			want: result{
+				statusCode: http.StatusCreated,
+				body:       `{"data":"testdata"}` + "\n",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test returns an error when trying to encode invalid input to json",
+			resp: NewResponse(http.StatusCreated, make(chan int)),
+			want: result{
+				statusCode: http.StatusCreated,
+				body:       `{"data":"testdata"}` + "\n",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := respond(tt.args.w, tt.args.resp); (err != nil) != tt.wantErr {
-				t.Errorf("respond():\n error = %v\n wantErr = %v", err, tt.wantErr)
+			w := httptest.NewRecorder()
+
+			if err := respond(w, tt.resp); (err != nil) != tt.wantErr {
+				t.Fatalf("respond():\n error = %v\n wantErr = %v", err, tt.wantErr)
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			got := w.Result()
+			if got.StatusCode != tt.want.statusCode {
+				t.Errorf("respond(): unexpected status code:\n got = %v\n want = %v", got.StatusCode, tt.want.statusCode)
+			}
+
+			if v := got.Header.Get("Content-Type"); v != "application/json" {
+				t.Errorf("respond(): unexpected header:\n got = %v\n want = application/json", v)
+			}
+
+			body, err := io.ReadAll(got.Body)
+			if err != nil {
+				t.Fatalf("respond():\n failed to read response body = %v\n", err)
+			}
+
+			if v := string(body); v != tt.want.body {
+				t.Errorf("respond():\n got = %v\n want = %v", v, tt.want.body)
 			}
 		})
 	}
